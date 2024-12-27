6c48456b01808bf828c8a757abc22fee05ace931
Yudhajit <yudhajitchowdhury17@gmail.com>
2024-12-28 00:12:54 +0530

    😵‍💫

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema; //taking out an instance of the Schema method from mongoose.
 
@@ -12,5 +14,32 @@ const userSchema = new Schema({
     type: String,
   },
 });
+
+userSchema.statics.singup = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled"); //json error message which will be displayed on the front end
+  }
+
+  if (!validatorisEmail(email)) {
+    //returns true if the email valid
+    throw Error("Email is not valid");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password is not strong enough");
+  }
+
+  const exists = await this.findOne({ email }); //checking if the email is already used by another user.
+
+  if (exists) {
+    throw Error("Email already in use"); //we dont have access to response function
+  }
+
+  const salt = await bcrypt.genSalt(10); //generating a random string which will be attached to the password before hashing
+
+  const hash = await bcrypt.hash(password, salt); //hashing the password
+
+  const user = await this.create({ email, password: hash }); //creating a document in mongo
+};
 
 mondule.exports = mongoose.model("User", userSchema); //passing the userSchema which we created and the name of the model
