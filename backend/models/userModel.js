3cce16ee537c7a1079669e74fa9c6f78e28b81eb
Yudhajit <yudhajitchowdhury17@gmail.com>
2024-12-28 00:35:20 +0530

    🐞

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -15,31 +15,23 @@ const userSchema = new Schema({
   },
 });
 
-userSchema.statics.singup = async function (email, password) {
+userSchema.statics.signup = async function (email, password) {
   if (!email || !password) {
-    throw Error("All fields must be filled"); //json error message which will be displayed on the front end
+    throw Error("All fields must be filled");
   }
-
-  if (!validatorisEmail(email)) {
-    //returns true if the email valid
+  if (!validator.isEmail(email)) {
     throw Error("Email is not valid");
   }
-
   if (!validator.isStrongPassword(password)) {
     throw Error("Password is not strong enough");
   }
-
   const exists = await this.findOne({ email }); //checking if the email is already used by another user.
-
   if (exists) {
-    throw Error("Email already in use"); //we dont have access to response function
+    throw Error("Email already in use"); //we dont have access to reponse function
   }
-
-  const salt = await bcrypt.genSalt(10); //generating a random string which will be attached to the password before hashing
-
+  const salt = await bcrypt.genSalt(10); //generating a random string which will get attached to the password before hashing
   const hash = await bcrypt.hash(password, salt); //hashing the password
-
   const user = await this.create({ email, password: hash }); //creating a document in mongo
+  return user;
 };
-
-mondule.exports = mongoose.model("User", userSchema); //passing the userSchema which we created and the name of the model
+module.exports = mongoose.model("User", userSchema);
